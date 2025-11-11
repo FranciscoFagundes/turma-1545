ae0b0d24581befaae5f29fa89f92812cbc51fee4
Francisco Fagundes <fagundesfrancisco@hotmail.com>
2025-11-11 19:17:15 +0000

    Primeiro commit

---
+++ aula10_11/index.js
@@ -0,0 +1,57 @@
+// var nome = "Francisco";
+
+// nome = "Maria";
+
+// nome = 5;
+
+// console.log(nome); 
+
+// Tipos nativos do JavaScript 
+
+// string, number, boolean, null, undefined, object, NaN 
+
+
+// let => define as variáveis
+// const => define as constantes
+
+// const PI = 3.14;
+// const PESO = 2;
+
+// PI = 5;
+
+// let notaDoAluno01 = 7;
+// let notaDoAluno02 = 6;
+
+// let notaFinal = (notaDoAluno01 * PESO) + notaDoAluno02;
+
+
+// const numero01 = "10.2";
+// const numero02 = 2;
+
+// console.log(numero01 + numero02);
+
+// console.log(Number(numero01) + numero02);
+
+// console.log(parseInt(numero01) + numero02);
+
+// console.log(parseFloat(numero01) + numero02);
+
+// console.error("Isso é um erro!");
+
+// console.info("Isso é uma informação");
+
+// console.warn("Isso é um aviso!");
+
+// console.table(["Azul", "Vermelho", "Amarelo"]);
+
+
+// const id = Symbol('id');
+// const user = { [id] : 123};
+
+// console.log(typeof(id));
+
+// let nome = "Francisco";
+
+// console.log(typeof(nome));
+
+
---
+++ aula11_11/index.js
@@ -0,0 +1,69 @@
+//console.log("Olá mundo!");
+
+// let nome = "Rodrigo";
+
+// let nomeAlterado = nome.replaceAll("o", "a");
+
+// console.log(nomeAlterado);
+
+
+
+// let nome = "Francisca";
+// let sobrenome = "Fagundes";
+
+
+//console.log(nome + " " +  sobrenome);
+
+// console.log(`${nome} ${sobrenome}`);
+
+//console.log(nome.length);
+//console.log(nome.trim().length)
+
+// console.log(nome.toUpperCase());
+
+// console.log(nome.toLowerCase());
+
+//console.log(nome.trim() + sobrenome);
+
+// trimStart() e trimEnd()
+
+//console.log(nome.replace(" ", ""));
+
+//console.log(nome.slice(0, 3));
+
+//console.log(nome.substring(2, 4));
+
+
+// console.log(nome.indexOf("a"));
+// console.log(nome.lastIndexOf("a"));
+
+
+//let fruta = "banana";
+
+// console.log(fruta.includes("na"));
+
+// console.log(fruta.startsWith("b"));
+
+// console.log(fruta.endsWith("a"));
+
+// console.log(fruta.split("a"));
+
+// let nome = "Francisco/Fagundes";
+
+// console.log(nome.split("/"));
+
+
+// let nome = "Francisco ";
+
+// let sobrenome = "Fagundes";
+
+
+// console.log(nome.concat(sobrenome));
+
+
+// let letras = ["F", "r", "a", "n"];
+
+// console.log(letras.join(""));
+
+
+
